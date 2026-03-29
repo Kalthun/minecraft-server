47fec40eb5f4a733dcc04bfa2be5cb4b28754cad
James kalthun McDonagh <jameskwmail@gmail.com>
2026-03-29 17:49:23 -0400

    adding the auto part

--- hosts/default/minecraft.nix
+++ hosts/default/minecraft.nix
@@ -10,7 +10,35 @@
       savage = import ../../servers/savage/server.nix { inherit pkgs inputs; };
       # test = import ../../servers/test/server.nix { inherit pkgs inputs; };
     };
+  };
 
+  systemd.services.minecraft-backup-savage = {
+    description = "Backup Minecraft Server: savage";
+
+    serviceConfig = {
+      Type = "oneshot";
+      User = "minecraft";
+      ExecStart = "${pkgs.bash}/bin/bash ${../../scripts/backup.sh} savage 25575 password";
+    };
+
+    path = with pkgs; [
+      coreutils
+      findutils
+      gnutar
+      gzip
+      mcrcon
+    ];
+  };
+
+  systemd.timers.minecraft-backup-savage = {
+    description = "Run backups for savage every 6 hours";
+    wantedBy = [ "timers.target" ];
+
+    timerConfig = {
+      Unit = "minecraft-backup-savage.service";
+      OnCalendar = [ "00:00" "06:00" "12:00" "18:00" ];
+      Persistent = true;
+    };
   };
 
 }
